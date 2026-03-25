01d965e668f5c0207a48fda2b5f517e8e889d476
George Nijo <george.nijo8@gmail.com>
2026-03-25 14:54:05 -0400

    feat: add PageHero component (#36) (#32)

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,7 @@
 export { Button } from './Button'
 export type { ButtonProps } from './Button'
 export { GoldDivider } from './GoldDivider'
+export { PageHero } from './PageHero'
+export type { PageHeroProps } from './PageHero'
 export { SectionHeader } from './SectionHeader'
 export type { SectionHeaderProps } from './SectionHeader'
